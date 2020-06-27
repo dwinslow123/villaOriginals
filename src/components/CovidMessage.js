f2884feca3214eb80db53463b2f547190bd2d520
Dan Winslow <Dwinslow123@me.com>
2020-06-27 12:51:25 -0500

    updated covid message

--- src/components/CovidMessage.js
+++ src/components/CovidMessage.js
@@ -1,18 +1,15 @@
-import React from 'react';
+import React from "react";
 import {
   Alert,
   Modal,
   ModalHeader,
   ModalBody,
   ModalFooter,
-  Button
-} from 'reactstrap';
+  Button,
+} from "reactstrap";
 
 const CovidModal = (props) => {
-  const {
-    buttonLabel,
-    className
-  } = props;
+  const { buttonLabel, className } = props;
 
   const [modal, setModal] = React.useState(false);
 
@@ -20,33 +17,42 @@ const CovidModal = (props) => {
 
   return (
     <div>
-      <Alert color="info">See our updates regarding COVID-19<Button color="secondary" onClick={ toggle } className={ className }>{ buttonLabel }</Button></Alert>
-      <Modal isOpen={ modal } toggle={ toggle } className="">
-        <ModalHeader toggle={ toggle }>COVID-19 Updates</ModalHeader>
+      <Alert color="info">
+        See our updates regarding COVID-19
+        <Button color="secondary" onClick={toggle} className={className}>
+          {buttonLabel}
+        </Button>
+      </Alert>
+      <Modal isOpen={modal} toggle={toggle} className="">
+        <ModalHeader toggle={toggle}>COVID-19 Updates</ModalHeader>
         <ModalBody>
-        <p>We will be reaching out to each of you to reschedule your appointments. We will start with the missed appointments from March and move on through April and May.</p>
-        <p>We will be temporarily extending our hours to accommodate the overflow of appointments. The salon will be operating at 50% capacity to maintain social distancing so please be patient, as we do our best to fit our guests in for appointments.</p> 
-        <p>Our number one priority is the health and safety of our guests, the team, and our community.</p>
-        <p>In the meantime we ask:</p>
-        <ol>
-          <li>If you are feeling ill or someone near you is ill, please reschedule your appointment.</li>
-          <li>Please do not bring any additional guests with you to your appointment.</li>
-          <li>Our waiting are will be closed. Please wait in your car until we are ready for you.</li>
-          <li>Please bring minimal belongings with you for your appointment.</li>
-          <li>Please use the hand sanitizer that will be provided and wash your hands as often as possible.</li>
-          <li>By entering the salon, you are stating that you are fever free and have no symptoms of illness.</li>
-          <li>Staff and guests are required to wear a mask or face covering. Please keep your mask on for the duration of your appointment.</li>
-          <li>Guests will be seated at every other station to maintain social distance.</li>
-          <li>Hand sanitizer will be provided at each station.</li>
-          <li>We will taking extra sanitary precautions and utilizing the time between guests to sanitize and clean.</li>
-        </ol>
+          <p>
+            As we enter Phase 4 or Restore Illinois, the guidelines have not
+            changed much for us:</p>
+            <p>A face mask/covering is required to enter the salon and we
+            are still limiting the amount of people in our establishment at one
+            time.<br />
+            <strong>
+              Capacity numbers have increased slightly according to the
+              guidelines.
+            </strong>
+          </p>
+          <p>
+            Our waiting room is now open but please be courteous about giving
+            people their space. Please check in the with front desk and we will advise if you need
+            to wait outside on the bench.
+            <br />
+          </p>
+          <p>Thank you all for your patience during this time, and we look forward to continuing to serve you!</p>
         </ModalBody>
         <ModalFooter>
-          <Button color="secondary" onClick={ toggle }>Exit</Button>
+          <Button color="secondary" onClick={toggle}>
+            Exit
+          </Button>
         </ModalFooter>
       </Modal>
     </div>
   );
-}
+};
 
 export default CovidModal;
