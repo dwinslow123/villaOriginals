bcecd853e57433d78363206d851cf91cb7fef956
Dan Winslow <dwinslow@nexient.com>
2025-10-08 12:05:13 -0500

    fixed spacing in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ import styles from './Header.module.css';
 const Header = () => {
     return (
         <>
-            <Navbar className={ styles.header }>
+            <Navbar className={styles.header}>
                 <NavbarBrand href='/'>
                     Villa Originals
                 </NavbarBrand>
